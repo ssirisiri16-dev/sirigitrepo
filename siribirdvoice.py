8d5a79d1eed3b0a8650f84239ff5927ae52f90b0
ssirisiri16-dev <ssirisiri16@gmail.com>
2026-05-26 17:45:14 +0530

    Add files via upload

--- siribirdvoice.py
+++ siribirdvoice.py
@@ -7,6 +7,74 @@ import tempfile
 st.set_page_config(page_title="Bird Sound Detector", layout="wide")
 
 st.title("🐦 Bird Sound Detection App")
+st.markdown("""
+<h5 class="title">
+🐦 Bird Sound Detection App
+</h5>
+
+<style>
+
+.title {
+    text-align: center;
+    font-size: 55px;
+    color: white;
+    font-weight: bold;
+
+    padding: 20px;
+
+    border-radius: 20px;
+
+    background: linear-gradient(to right, #ff512f, #dd2476);
+
+    box-shadow: 0px 0px 30px rgba(255,255,255,0.8);
+
+    animation: pop 2s infinite;
+}
+
+/* Pop Animation */
+@keyframes pop {
+
+    0% {
+        transform: scale(1);
+    }
+
+    50% {
+        transform: scale(1.08);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+/* Main App Background */
+.stApp {
+    background-color: pink;
+}
+
+
+/* Browse Files Button */
+.stFileUploader button {
+    background-color:maroon;
+    color: white;
+    border-radius: 10px;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+}
+
+/* Hover Effect */
+.stFileUploader button:hover {
+    background-color: darkred;
+    color: yellow;
+}
+
+
+</style>
+
+<div class="bird">🕊️</div>
+
+""", unsafe_allow_html=True)
+
 
 model = joblib.load("bird_sound_model.pkl")
 
